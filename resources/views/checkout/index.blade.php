c87b4dc34d8a7756a28eead949f1c7ada16b5068
Jayank Tiwari <jayank226633@gmail.com>
2025-10-15 23:39:04 +0530

    Changes in delivery rates

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -216,7 +216,7 @@
                 
                 // Delivery charges mapping
                 const deliveryCharges = {
-                    '08880': 0.00,
+                    '08880': 3.00,
                     '08800': 0.00,
                     '08812': 2.00,
                     '08870': 4.00
